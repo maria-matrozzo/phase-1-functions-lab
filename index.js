f4e1741d4f17293d77f984452f20f7f9c0c659a2
Maria <mmatrozzo@gmail.com>
2021-11-02 21:03:52 -0400

    commit tree 1368aa1683ead171d78ce87f855ca4644890eb3e

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+let num1 = 42
+function distanceFromHqInBlocks(pickup) {
+    if (pickup > 42) {
+        return (pickup - num1);
+    } else if (pickup < 42) {
+        return (num1 - pickup);
+    }
+}
+
+let feet = 264
+function distanceFromHqInFeet(pickup) {
+    return distanceFromHqInBlocks(pickup) * feet
+    }
+
+    function distanceTravelledInFeet (headquarters, num2){
+        if (num2 > headquarters){
+            return (num2 - headquarters)*feet;
+    
+        }else{
+        return (headquarters - num2)*feet;
+        }
+    }
+
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+        return 0
+    } else if (distance < 2001) {
+        return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance < 2500) {
+        return 25
+    } else if (distance > 2500) {
+        return 'cannot travel that far';
+    }
+}
+calculatesFarePrice(34,32) 
+calculatesFarePrice(50,58)
+calculatesFarePrice(34,24)
